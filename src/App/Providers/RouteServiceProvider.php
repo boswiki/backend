ec8dea79dca90da858dad1aa45dd4ea44c4446eb
wellá <info@lost.design>
2023-09-20 18:20:45 +0200

    refactor: change api routes
    remove /api from routes, disable web routes and fix login throttle

--- src/App/Providers/RouteServiceProvider.php
+++ src/App/Providers/RouteServiceProvider.php
@@ -30,11 +30,10 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+//            Route::middleware('web')
+//                ->group(base_path('routes/web.php'));
         });
     }
 
