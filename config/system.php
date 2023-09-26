5761fc930c17727d6c364bb652968c21d5901c77
wellá <info@lost.design>
2023-09-26 12:23:55 +0200

    feat: add system user for imports

---
+++ config/system.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'user' => [
+        'username' => env('SYSTEM_USER_USERNAME'),
+        'email' => env('SYSTEM_USER_EMAIL'),
+        'password' => env('SYSTEM_USER_PASSWORD')
+    ]
+];
