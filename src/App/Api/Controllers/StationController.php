da96eb28ffc2948fde69f8acada6087b10af79cd
wellá <info@lost.design>
2023-09-26 12:25:39 +0200

    refactor: remove unused code

--- src/App/Api/Controllers/StationController.php
+++ src/App/Api/Controllers/StationController.php
@@ -15,32 +15,13 @@ class StationController extends Controller
 {
     public function index(Request $request)
     {
-
-        $stations = Station::query()
-            ->select('*')
-            ->paginate(30);
-
-        return StationIndexResource::collection($stations);
+        return StationIndexResource::collection(Station::query()->paginate(30));
     }
 
     public function show(Request $request, Station $station)
     {
-
-//        $district = DB::table('districts')->where('id', $station->district_id)->first();
-//        $address = DB::table('addresses')->where('id', $station->address_id)->first();
-//        $stationType = DB::table('station_types')->where('id', $station->station_type_id)->first();
-//        $controlCenter = DB::table('control_centers')->where('id', $station->control_center_id)->first();
-//
-//        $station->setRelation('district', $district);
-//        $station->setRelation('address', $address);
-//        $station->setRelation('stationType', $stationType);
-//        $station->setRelation('controlCenter', $controlCenter);
-//
-////        dd($station);
-//
-//        dd($district, $station->toArray());
-
-        $station->load('district', 'address', 'stationType', 'controlCenter');
-        return StationResource::make($station);
+        return StationResource::make(
+            $station->load('author', 'district', 'address', 'stationType', 'controlCenter')
+        );
     }
 }
